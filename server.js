8c4ab1e6288d1a87f0fcf6d880bafce06288e4f5
Himanshu Shekhar <himanshu.shekhar@cashfree.com>
2025-07-24 01:32:07 +0530

    add env to other api

--- server.js
+++ server.js
@@ -57,9 +57,8 @@ app.post("/verifyPayment", async (req, res) => {
     method: "GET",
     headers: {
       "x-api-version": "2025-01-01",
-      "x-client-id": "TEST1055024968f0e6fde20ede21146894205501",
-      "x-client-secret":
-        "cfsk_ma_test_5d71f2ea46b93f7ea1635c94ec3b80ab_4de6af65",
+      "x-client-id": process.env.APP_ID,
+      "x-client-secret": process.env.APP_SECRET,
       "Content-Type": "application/json",
     },
   };
