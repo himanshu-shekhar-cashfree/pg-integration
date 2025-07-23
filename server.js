3ba266d2edec8d1e77542480631b9c281fb97a6d
Himanshu Shekhar <himanshu.shekhar@cashfree.com>
2025-07-24 02:40:05 +0530

    add cors

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 require("dotenv").config();
 
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use("/assets", express.static("assets"));
